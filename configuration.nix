64f454dff06901de7a52e703e5b550e3c715240c
Dirakon <609700zxc@mail.ru>
2024-04-20 01:24:20 +0300

    some cleanup

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,7 @@
       ./hardware-configuration.nix
     ];
 
+    # For obsidian
     nixpkgs.config.permittedInsecurePackages = [
               "electron-25.9.0"
     ];
--- home.nix
+++ home.nix
@@ -4,13 +4,10 @@
   home.username = "dirakon";
   home.homeDirectory = "/home/dirakon";
 
-  # link the configuration file in current directory to the specified location in home directory
-  #home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   home.file.".config" = {
         source = ./.config;
         recursive = true;
   };
- # home.file.".config/fish/config.fish".source = ./fish/config.fish;
   home.file."bin/nix-command-not-found" = {
         text = ''
           #!/usr/bin/env bash
@@ -21,89 +18,11 @@
          executable = true;
        };
 
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # set cursor size and dpi for 4k monitor
- #  xresources.properties = {
- #    "Xcursor.size" = 16;
- #    "Xft.dpi" = 172;
- #  };
 
   # Packages that should be installed to the user profile.
 #  home.packages = with pkgs; [
-#    # here is some command line tools I use frequently
-#    # feel free to add your own or remove some of them
-#
 #  ];
 
- # # basic configuration of git, please change to your own
- # programs.git = {
- #   enable = true;
- #   userName = "Ryan Yin";
- #   userEmail = "xiaoyin_c@qq.com";
- # };
-
- # # starship - an customizable prompt for any shell
- # programs.starship = {
- #   enable = true;
- #   # custom settings
- #   settings = {
- #     add_newline = false;
- #     aws.disabled = true;
- #     gcloud.disabled = true;
- #     line_break.disabled = true;
- #   };
- # };
-
- # # alacritty - a cross-platform, GPU-accelerated terminal emulator
- # programs.alacritty = {
- #   enable = true;
- #   # custom settings
- #   settings = {
- #     env.TERM = "xterm-256color";
- #     font = {
- #       size = 12;
- #       draw_bold_text_with_bright_colors = true;
- #     };
- #     scrolling.multiplier = 5;
- #     selection.save_to_clipboard = true;
- #   };
- # };
-
- # programs.bash = {
- #   enable = true;
- #   enableCompletion = true;
- #   # TODO add your custom bashrc here
- #   bashrcExtra = ''
- #     export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
- #   '';
-
- #   # set some aliases, feel free to add more or remove some
- #   shellAliases = {
- #     k = "kubectl";
- #     urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
- #     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
- #   };
- # };
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
   programs.neovim = {
     enable = true;
     extraConfig = ''
@@ -114,15 +33,6 @@
   programs.neovim.plugins = [
     pkgs.vimPlugins.nvim-treesitter.withAllGrammars
   ];
-
-#  programs.nix-index =
-#  {
-#    enable = true;
-#    enableFishIntegration = true;
-#  };
-
-  
-
 
   home.stateVersion = "23.11";
 
