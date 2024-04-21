9e8a59405b2c95798e4e7d333ffb09f18a5b6e55
Dirakon <609700zxc@mail.ru>
2024-04-22 01:34:24 +0300

    fix battery numbers

--- .scripts/battery-monitor.sh
+++ .scripts/battery-monitor.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 #
 # Set the battery level threshold
-BATTERY_THRESHOLD=98
+BATTERY_THRESHOLD=20
 
 # Function to check battery level
 check_battery() {
     BATTERY_PATH=$(upower -e | grep battery)
     LINE_POWER_PATH=$(upower -e | grep line_power)
     BATTERY_PERCENTAGE=$(upower -i $BATTERY_PATH | grep 'percentage:' | awk '{ print $2 }' | sed 's/%//')
+    CABLE_PLUGGED=$(upower -i $LINE_POWER_PATH | grep -A2 'line-power' | grep online | awk '{ print $2 }')
 
     # Check if battery level is below the threshold
     if [ "$BATTERY_PERCENTAGE" -lt "$BATTERY_THRESHOLD" ]; then
-        # Send an urgent notification
-        notify-send "Battery Low" "Your battery level is below $BATTERY_THRESHOLD%. Please plug in your charger." -u critical
+        if [[ $CABLE_PLUGGED == 'no' ]]; then
+            # Send an urgent notification
+            notify-send "Battery Low" "Your battery level is below $BATTERY_THRESHOLD%. Please plug in your charger." -u critical
+        fi
     fi
 }
 
