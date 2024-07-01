e4bc423e922305ddde687218c4b3a933618918f8
Dirakon <609700zxc@mail.ru>
2024-07-01 21:42:04 +0300

    some fixes

--- modules/guide/default.nix
+++ modules/guide/default.nix
@@ -32,39 +32,9 @@ self@{ config, pkgs, boot, stable, hostname, modulesPath, ... }:
 
 
   environment.systemPackages = with pkgs; [
-    # some cli tools
-    # vim
-    # wget
     # gitMinimal # Temporary
-    # git
-    # lshw
-    # zip
-    # unzip
-    # ripgrep
-    # htop
     curl
-
-    # Nix stuff
-    # nh
-
-    # Docker stuff
-    # docker-compose
   ];
-
-  # virtualisation.docker.enable = true;
-
-  # programs.fish.enable = true;
-  # users.defaultUserShell = pkgs.fish;
-
-  # zramSwap = {
-  #   enable = true;
-  #   memoryPercent = 100;
-  # };
-
-  # swapDevices = [ {
-  #   device = "/var/lib/swapfile";
-  #   size = 2*1024; # 2 gigs
-  # } ];
 
   networking.hostName = "${hostname}";
 }
