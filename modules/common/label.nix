7e07a44f4da295b8ff3220161d594e21482b696c
Dirakon <609700zxc@mail.ru>
2025-04-11 21:05:24 +0300

    Update tmux

--- modules/common/label.nix
+++ modules/common/label.nix
@@ -1,1 +1,1 @@
-"Nvim_update"
+"Update_tmux"
--- modules/crusader/tmux.nix
+++ modules/crusader/tmux.nix
@@ -1,4 +1,23 @@
 self@{ config, pkgs, boot, hostname, sensitive, modulesPath, lib, ... }:
+let
+  navigatorScript = pkgs.writeShellScriptBin "navigator.sh" ''
+    set -e
+
+    cmd="$(tmux display -p '#{pane_current_command}')"
+    cmd="$(basename "$cmd" | tr A-Z a-z)"
+    pane_count="$(tmux list-panes | wc -l)"
+    # echo $cmd
+
+    if [ "''${cmd%m}" = "vi" ] || [ "''${cmd%m}" = "dev" ] || [ "''${cmd%m}" = "nvi" ] || [ "$pane_count" -eq 1 ]; then
+      direction="$(echo "''${1#-}" | tr 'lLDUR' '\\hjkl')"
+      tmux send-keys "C-$direction"
+      # echo "send C-$direction"
+    else
+      tmux select-pane "$@"
+      # echo "not send"
+    fi
+  '';
+in
 {
   programs.tmux = {
     enable = true;
@@ -48,21 +67,18 @@ self@{ config, pkgs, boot, hostname, sensitive, modulesPath, lib, ... }:
       unbind C-Left 
       unbind C-Right
 
-      # inline of vim-tmux-navigator
-      # Smart pane switching with awareness of Vim splits.
-      # See: https://github.com/christoomey/vim-tmux-navigator
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-          | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|dev|fzf)(diff)?$'"
-      bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
-      bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
-      bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
-      bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
-      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+      # tmux now opens splits in the current directory
+      bind % split-window -h -c "#{pane_current_path}"
+      bind '"' split-window -v -c "#{pane_current_path}"
 
+      # Smart pane switching with awareness of Vim splits.
+      bind-key -n 'C-k' run-shell '${navigatorScript}/bin/navigator.sh -U'
+      bind-key -n 'C-j' run-shell '${navigatorScript}/bin/navigator.sh -D'
+      bind-key -n 'C-h' run-shell '${navigatorScript}/bin/navigator.sh -L'
+      bind-key -n 'C-l' run-shell '${navigatorScript}/bin/navigator.sh -R'
+      bind-key -n "C-\\" run-shell '${navigatorScript}/bin/navigator.sh -l'
+
+      # And it(^) continues to work in copy-mode-vi
       bind-key -T copy-mode-vi 'C-h' select-pane -L
       bind-key -T copy-mode-vi 'C-j' select-pane -D
       bind-key -T copy-mode-vi 'C-k' select-pane -U
